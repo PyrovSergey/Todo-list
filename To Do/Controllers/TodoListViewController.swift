c319385079f3cfd2ee065154b0e8147ccb3a2df3
serge <pyrov.sergey@gmail.com>
2019-02-04 12:58:03 +0300

    Persisting data using Codable protocol and small refactoring code

--- To Do/Controllers/TodoListViewController.swift
+++ To Do/Controllers/TodoListViewController.swift
@@ -16,73 +16,7 @@ class TodoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         loadItems()
-        // Data for testing
-//        let item1 = TodoItem()
-//        item1.title = "1"
-//        item1.isDone = true
-//        itemArray.append(item1)
-//
-//        let item2 = TodoItem()
-//        item2.title = "2"
-//        itemArray.append(item2)
-//
-//        let item3 = TodoItem()
-//        item3.title = "3"
-//        itemArray.append(item3)
-//
-//        let item4 = TodoItem()
-//        item4.title = "4"
-//        itemArray.append(item4)
-//
-//        let item5 = TodoItem()
-//        item5.title = "5"
-//        itemArray.append(item5)
-//
-//        let item6 = TodoItem()
-//        item6.title = "6"
-//        itemArray.append(item6)
-//
-//        let item7 = TodoItem()
-//        item7.title = "7"
-//        itemArray.append(item7)
-//
-//        let item8 = TodoItem()
-//        item8.title = "8"
-//        itemArray.append(item8)
-//
-//        let item9 = TodoItem()
-//        item9.title = "9"
-//        itemArray.append(item9)
-//
-//        let item10 = TodoItem()
-//        item10.title = "10"
-//        itemArray.append(item10)
-//
-//        let item11 = TodoItem()
-//        item11.title = "11"
-//        itemArray.append(item11)
-//
-//        let item12 = TodoItem()
-//        item12.title = "12"
-//        itemArray.append(item12)
-//
-//        let item13 = TodoItem()
-//        item13.title = "13"
-//        itemArray.append(item13)
-//
-//        let item14 = TodoItem()
-//        item10.title = "14"
-//        itemArray.append(item14)
-//
-//        let item15 = TodoItem()
-//        item11.title = "15"
-//        itemArray.append(item15)
-//
-//        let item16 = TodoItem()
-//        item12.title = "16"
-//        itemArray.append(item16)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -118,7 +52,6 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add item", style: .default) {
             (action) in
-            //
             
             if let resultTextInput = inputTextItem.text {
                 let newTodoItem = TodoItem()
